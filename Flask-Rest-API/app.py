a52bdff666c8c4dd2dad87136c8b00927ec4f298
xamzzs <samiraraah18@gmail.com>
2025-12-13 14:44:28 +0800

    configure JWTManager

--- Flask-Rest-API/app.py
+++ Flask-Rest-API/app.py
@@ -1,12 +1,19 @@
+from datetime import timedelta
+import MySQLdb
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_mysqldb import MySQL
 from config import MYSQL_CONFIG
 
 app = Flask(__name__)
 app.config.update(MYSQL_CONFIG)
+app.config['JWT_SECRET_KEY'] = 'change-this-key'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=30)
 
 mysql = MySQL(app)
+jwt = JWTManager(app)
 
+VALID_USER = {"username": "admin", "password": "admin123"}
 
 @app.route('/')
 def home():
